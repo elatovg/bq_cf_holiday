88526b3483cccc385594b2052515e94a8271bfd2
elatov <elatov@google.com>
2020-07-22 16:39:35 -0400

    h

--- python/main.py
+++ python/main.py
@@ -3,6 +3,25 @@ from datetime import date
 import base64
 import os
 
+def hello_pubsub(event, context):
+    """Background Cloud Function to be triggered by Pub/Sub.
+    Args:
+         event (dict):  The dictionary with data specific to this type of
+         event. The `data` field contains the PubsubMessage message. The
+         `attributes` field will contain custom attributes if there are any.
+         context (google.cloud.functions.Context): The Cloud Functions event
+         metadata. The `event_id` field contains the Pub/Sub message ID. The
+         `timestamp` field contains the publish time.
+    """
+    print("""This Function was triggered by messageId {} published at {}
+    """.format(context.event_id, context.timestamp))
+
+    if 'data' in event:
+        name = base64.b64decode(event['data']).decode('utf-8')
+    else:
+        name = 'World'
+    print('Hello {}!'.format(name))
+
 def start_function(event, context):
     print("We get into here")
     if (context == "local"):
